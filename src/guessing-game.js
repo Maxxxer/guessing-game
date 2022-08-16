d8ea425a740b5f0d9c08b87f82179b020d6e47d6
Maksim <polonets.maksim@gmail.com>
2022-08-16 10:58:19 +0300

    init: guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
-    }
+        this.max = max;
+        this.min = min;
+      }
 
     guess() {
-
-    }
+        return Math.round((this.max + this.min) / 2);
+        }
 
     lower() {
-
-    }
+        this.max = ((Math.round((this.max + this.min)/2)));
+        }
 
     greater() {
+        this.min = ((Math.round((this.max + this.min)/2)));
+        }
 
-    }
 }
 
 module.exports = GuessingGame;
